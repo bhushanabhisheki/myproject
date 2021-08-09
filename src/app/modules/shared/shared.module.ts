72d5e2259b5a98405a96bba6fcf9e821cf1b6fe1
Vyankatesh Abhisheki <bhushanabhisheki@gmail.com>
2021-08-09 18:31:57 +0530

    added Gridster support for drag and drop

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 // import all third party module here
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularSlickgridModule } from 'angular-slickgrid';
+import { GridsterModule } from 'angular-gridster2';
 
 // import all angular material module here
 import { MatIconModule } from '@angular/material/icon';
@@ -23,7 +24,8 @@ import { ThemeSwitchComponent } from './components/theme-switch/theme-switch.com
     MatDividerModule,
     MatButtonToggleModule,
     FlexLayoutModule,
-    AngularSlickgridModule
+    AngularSlickgridModule,
+    GridsterModule
   ],
   exports: [
     ThemeSwitchComponent,
@@ -32,7 +34,8 @@ import { ThemeSwitchComponent } from './components/theme-switch/theme-switch.com
     MatDividerModule,
     MatButtonToggleModule,
     FlexLayoutModule,
-    AngularSlickgridModule
+    AngularSlickgridModule,
+    GridsterModule
   ]
 })
 export class SharedModule {}
