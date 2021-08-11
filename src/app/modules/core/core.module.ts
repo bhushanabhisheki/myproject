a1f5841daa607a3c11737b2ba77adde53b1be078
Vyankatesh Abhisheki <bhushanabhisheki@gmail.com>
2021-08-11 12:05:11 +0530

    added sub header and main application body

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -4,9 +4,17 @@ import { SharedModule } from '../shared/shared.module';
 import { CoreLayoutComponent } from './components/core-layout/core-layout.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainBodyComponent } from './components/main-body/main-body.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
 
 @NgModule({
-  declarations: [CoreLayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    CoreLayoutComponent,
+    HeaderComponent,
+    SubHeaderComponent,
+    MainBodyComponent,
+    FooterComponent
+  ],
   imports: [SharedModule],
   exports: [CoreLayoutComponent]
 })
